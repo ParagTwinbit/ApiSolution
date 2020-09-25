72c28effa80b3ccbd8b59e1493acbb77fa77bd2c
ParagTwinbit <parag.twinbit@gmail.com>
2020-09-25 11:08:20 +0600

    Initial Commit

---
+++ Solution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Solution
+//
+//  Created by Twinbit Test on 25/9/20.
+//  Copyright © 2020 Twinbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
